5d8a82bf6c2b40fcdf5c4b9fc7b4c5701a2792ee
kristian maulana <kristian.maulana@outlook.com>
2025-04-30 15:12:25 +0700

    [FIX]PAM:fixing error refer :  e8718d30

--- customize/santosa-project/santosa_hr_training/__manifest__.py
+++ customize/santosa-project/santosa_hr_training/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','hr','sanbe_hr_extended','sanbe_hr_extended'],
 
     # always loaded
     'data': [
