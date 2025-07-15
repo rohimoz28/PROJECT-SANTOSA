819987df78f414499b0c29fc2a33d484880f1c05
kristian maulana <kristian.maulana@outlook.com>
2025-07-15 09:16:26 +0700

    [FEAT]PAM : Master Data Unit Group Pelayanan

--- customize/santosa-project/sanbe_hr_extended/models/emp_group.py
+++ customize/santosa-project/sanbe_hr_extended/models/emp_group.py
@@ -37,3 +37,32 @@ class EmpGroup(models.Model):
               elif group.code and group.name and not group.responsible_id:
                 name = '[' +  group.code +'] + ' + group.name
               group.display_name = name
+
+
+class ServiceGroupsMST(models.Model):
+    _name = 'mst.group.pelayanan'
+    _description = "Master Group Pelayanan"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'code'
+
+    name = fields.Char('Name',required=True,tracking=True)
+    code = fields.Char('Code',required=True,size=6,tracking=True)
+    active = fields.Boolean('Active',default=True,tracking=True)
+    # branch_id = fields.Many2one('res.branch',string='Bisnis Unit',
+    #   default=lambda self: self.env.user.branch_id,)
+    _sql_constraints = [
+        ('name_uniq', 'unique(name)', 'The name must be unique!'),
+        ('code_uniq', 'unique(code)', 'The code must be unique!'),
+    ]
+
+    @api.depends('name', 'code')
+    def _compute_display_name(self):
+        for profesion in self:
+            name = ''
+            if profesion.code and profesion.name:
+                name = '[' +  profesion.code +'] ' + profesion.name
+            profesion.display_name = name
+
+    # @api.model
+    def unlink(self):
+        return super(ServiceGroupsMST, self).unlink()
