317a8cc5a5e5af05c08f905902ca958e30f63cda
kristian_maulana <kristian_maulana>
2025-12-16 14:36:05 +0700

    [FIX]2512001 - TMS : refisi paramnya untuk domain

--- customize/santosa-project/sanbe_hr_tms/models/hr_overtime_request_planning.py
+++ customize/santosa-project/sanbe_hr_tms/models/hr_overtime_request_planning.py
@@ -201,28 +201,26 @@ class HREmpOvertimeRequest(models.Model):
     #                                  domain="[('branch_id','=',branch_id), ('hrms_department_id', 'in',  [97, 174, 235]), ('user_id','!=', False)]",
     #                                  store=True, index=True)
     approverhrd_id = fields.Many2one('hr.employee', string='Approval by HRD',
-                                     domain="[('branch_id','=',branch_id), ('user_id','!=', False)]",
+                                     domain="[('branch_id','=',branch_id),('hrms_department_id','=',hrms_department_id), ('user_id','!=', False)]",
                                      store=True, index=True)
-    @api.model
-    def _get_splhrd_ids(self):
-        """Fetch the list of HRD department IDs from system parameter"""
-        # param = self.env['ir.config_parameter'].sudo().get_param('hr.overtime.planning')
-        param = self.env['ir.config_parameter'].sudo().get_param('SPLHRD')
-        return [int(x) for x in param.split(',')] if param else []
 
-    @api.onchange('branch_id')
-    def _onchange_branch_id(self):
-        splhrd_ids = self._get_splhrd_ids()
-        return {
-            'domain': {
-                'approverhrd_id': [
-                    ('branch_id', '=', self.branch_id.id),
-                    ('hrms_department_id', 'in', splhrd_ids),
-                    ('user_id', '!=', False),
-                ]
-            }
-        }
-    
+    approval_dept = fields.Many2one(
+        'sanhrms.department', string='Departemen', compute="_get_splhrd_ids")
+
+    def _get_splhrd_ids(self):
+        param = self.env['ir.config_parameter'].sudo().get_param('SPLHRD')
+        if param:
+            department_ids = [int(x.strip())
+                              for x in param.split(',') if x.strip().isdigit()]
+        else:
+            department_ids = []
+        approval_depts = self.env['sanhrms.department'].search(
+            [('id', 'in', department_ids)], limit=1)
+        if approval_depts:
+            return approval_depts
+        else:
+            raise UserError("Approval HRD doesn't set")
+
     approval_l1_id = fields.Many2one(
         comodel_name='hr.employee',
         string='Approval L1',
